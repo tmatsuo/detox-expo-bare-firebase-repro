d65a27d8f2c2e670fabfbf5c40c9b362c57d2099
Takashi Matsuo <matsuo.takashi@gmail.com>
2023-04-21 15:16:56 -0700

    downgraded to expo 47.0, react-native 0.70, changed to bare workflow

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} testID='homeView'>
+      <Text>A simple app for detox repro</Text>
       <StatusBar style="auto" />
     </View>
   );
